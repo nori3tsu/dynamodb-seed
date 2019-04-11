d645442453fd4679893c33cd3f78169589420c5f
Norimitsu Yamashita <tugend.licht@gmail.com>
2019-04-11 18:29:12 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# DynamoDB Seed
+
+This is a cli library that handling seed data for DynamoDB.
+
+## YAML format
+
+```
+---
+  -
+    TableName: "TableA"
+    Items:
+      -
+        id: "0001"
+        value: "0001"
+      -
+        id: "0002"
+        value: "0002"
+  -
+    TableName: "TableB"
+    Items:
+      -
+        id: "0001"
+        value: "0001"
+```
+
+## Usage
+
+```
+load a file
+$ dynamodb-seed -f ./seed.yaml
+
+load files
+$ dynamodb-seed -f ./**/*.yaml
+```
+
+## Options
+
+```
+$ dynamodb-seed --help
+Usage: dynamodb-seed --debug
+
+Options:
+  -f, --file <value>    Required: seed files using glob pattern
+  --region <value>      DynamoDB region (default: "ap-northeast-1")
+  -p, --prefix <value>  a table prefix
+  -s, --suffix <value>  a table suffix
+  --endpoint <value>    DynamoDB endpoint
+  --profile <value>     an AWS profile name
+  --debug               output debug logs
+  -h, --help            output usage information
+```
---
+++ bin/dynamodb-seed.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+"use strict";
+
+const fs = require("fs");
+const yaml = require('js-yaml');
+const glob = require('glob-promise');
+const program = require("commander");
+const AWS = require("aws-sdk");
+
+program
+  .usage("-f file")
+  .option("-f, --file <value>", "Required: seed files using glob pattern", String)
+  .usage("--region")
+  .option("--region <value>", "DynamoDB region", String, "ap-northeast-1")
+  .usage("-p, --prefix <value>")
+  .option("-p, --prefix <value>", "a table prefix", String)
+  .usage("-s, --suffix <value>")
+  .option("-s, --suffix <value>", "a table suffix", String)
+  .usage("--endpoint")
+  .option("--endpoint <value>", "DynamoDB endpoint", String)
+  .usage("--profile")
+  .option("--profile <value>", "an AWS profile name", String)
+  .usage("--debug")
+  .option("--debug", "output debug logs")
+  .parse(process.argv);
+
+const run = async () => {
+  if (!program.file) {
+    program.outputHelp();
+    process.exit(1);
+  }
+
+  const options = {};
+  if (program.region) options.region = program.region;
+  if (program.endpoint) options.endpoint = program.endpoint;
+
+  if (program.profile) {
+    const credentials = new AWS.SharedIniFileCredentials({profile: program.profile});
+    AWS.config.credentials = credentials;
+  }
+
+  const client = new AWS.DynamoDB.DocumentClient(options);
+
+  const files = await glob(program.file)
+  files.forEach(async (file) => {
+    try {
+      if (!file.match(/\.(yml|yaml)$/)) {
+        return;
+      }
+
+      console.log(file);
+
+      const seeds = yaml.safeLoad(fs.readFileSync(file, "utf8"));
+      await seeds.map(async ({ TableName, Items }) => {
+        const tableName = [
+          program.prefix,
+          TableName,
+          program.suffix,
+        ].filter(s => s != undefined).join('');
+
+        console.log(`Load ${Items.length} items into the ${tableName}.`)
+
+        return Promise.all(Items.map(async (Item) => {
+          const params = { TableName: tableName, Item };
+
+          if (program.debug) console.log(params);
+          await client.put(params).promise();
+        })).catch((err) => {
+          console.log(`Failed to load items - ${err.message}`);
+        });
+      });
+    } catch (err) {
+      console.log(`Faild to parse ${file} - ${err.message}`)
+    }
+  });
+}
+
+run();
+
